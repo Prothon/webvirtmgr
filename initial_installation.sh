4428c26b2f54771f5d84143657dc3c23c6899755
raymer <AndrewRaymer@gmail.com>
2014-03-07 19:44:32 -0500

    Updated the requirements on installation. Minor updates to install script

--- initial_installation.sh
+++ initial_installation.sh
@@ -4,7 +4,7 @@ echo "This is for initial installation of the application. Currently heavily in 
 
 yum -y install http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 yum -y install git python-pip libvirt-python libxml2-python python-websockify supervisor nginx 
-yum install -y gcc python-devel python-setuptools
+yum install -y gcc gmp-devel python-devel python-setuptools
 
 
  iptables-save
