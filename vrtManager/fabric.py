a9b073c1e6e03b1ab99cb5338793cc3216f35c75
raymer <AndrewRaymer@gmail.com>
2014-03-07 22:41:30 -0500

    Trying another fabric module

--- vrtManager/fabric.py
+++ vrtManager/fabric.py
@@ -1,7 +1,16 @@
-from fabric import api
-#from fabric.network import disconnect_all
-from contextlib import contextmanager
+from django.utils.timezone import utc
+from fabric.context_managers import hide, settings
+from fabric.decorators import task, serial
+from fabric.operations import run, sudo, local
+from fabric.state import env
+from fabric.tasks import execute
+from skwissh.models import Server, Measure, MeasureDay, MeasureWeek, \
+    MeasureMonth, CronLog
+import datetime
+import kronos
+import threading
+import traceback
 
 def get_hostname(request):  
-    hostname = f.remote_server(hostname)
+    execute(hostname, hosts=localhost)
     return hostname
