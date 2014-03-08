bf48bce4decb60ff1f8826b7a0d23ed19a085bb3
raymer <AndrewRaymer@gmail.com>
2014-03-07 20:40:59 -0500

    I think my fabric.uptime function was returning wrong data the entire time

--- servers/views.py
+++ servers/views.py
@@ -141,6 +141,6 @@ def cman(request):
     if not request.user.is_authenticated():
         return HttpResponseRedirect('/login')
 
-    #fabrictest = fabric.uptime 
-    #print fabrictest
+    compute = Compute.objects.filter()
+    fabrictest = fabric.uptime()
     return render_to_response('cman.html', locals(), context_instance=RequestContext(request))
