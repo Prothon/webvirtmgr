865779c246f5c02483e23732808057b17d7a5d72
raymer <AndrewRaymer@gmail.com>
2014-03-08 10:02:30 -0500

    Forgot to include the changes to the library

--- vrtManager/sshfabric.py
+++ vrtManager/sshfabric.py
@@ -4,11 +4,14 @@ import paramiko
 import cmd
 
 class RunCommand(object):
-    def connect(self, hostip, username, password, port):
+    def connect(self, hostip, username, password, port, command):
         """Connect to all hosts in the hosts list"""
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(hostip, username = username, password = password, port=port)
+        (stdin, stdout, stderr) = client.exec_command(command)
+        stdin.close()
+        return stdin, stdout, stderr
 
     def run(self, command):
         """run Execute this command on all hosts in the list"""
